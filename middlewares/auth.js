43764581779beeb8268559a61d12b93a362eccb0
Sayyad-Arsalan <108569306+Sayyad-Arsalan@users.noreply.github.com>
2024-11-09 20:20:50 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,21 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  let token;
+
+  if (req.cookies.token) {
+    token = req.cookies.token;
+  } else if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
   if (!token) {
     return next(new ErrorHandler("User not Authenticated!", 400));
   }
+
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
   req.user = await User.findById(decoded.id);
+  
   next();
 });
